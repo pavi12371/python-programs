61e2340e99ae1a5358bb05c6915da8069a8c7e1a
pavi12371 <peetlapavithra52@gmail.com>
2025-04-04 03:47:37 -0700

    Add files via upload

---
+++ exp2.1.py
@@ -0,0 +1,6 @@
+ls=[john,somu,puru]
+age=int(input("enter your age:"))
+if age>=18:
+    print("congratulations",name+",you are eligible to vote!")
+else:
+    print("reject",name+",you are not eligible to vote!")
