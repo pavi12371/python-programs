47930480e8a465522d75671223b8047689bc22ed
pavi12371 <peetlapavithra52@gmail.com>
2025-04-04 03:15:02 -0700

    Add files via upload

---
+++ exp1.py
@@ -0,0 +1,8 @@
+a=67
+b=45
+print("the addition is:",a+b)
+print("the difference is :",a-b)
+print("the multiplication is:",a*b)
+print("the division is:",a/b)
+print("the modulu is :",a%b)
+print("the floor division:",a//b)
