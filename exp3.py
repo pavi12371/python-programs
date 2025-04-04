27a54416155b5c870c09cb5ccce8d36e581b22d5
pavi12371 <peetlapavithra52@gmail.com>
2025-04-04 04:05:58 -0700

    Add files via upload

---
+++ exp3.py
@@ -0,0 +1,6 @@
+authorizes_users=["admin","user1","user2"]
+username=input("enter your username:")
+if username in authorized_users:
+ print("authorized_users access is granted!")  
+else:
+ print(" authorized_users  is denined")    
