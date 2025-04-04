567490982a93a8de96f5c3f4dce086bff0f8f279
pavi12371 <peetlapavithra52@gmail.com>
2025-04-04 03:45:06 -0700

    Add files via upload

---
+++ exp2.py
@@ -0,0 +1,7 @@
+l=eva(inpur("enter the length of the rectangle:"))
+b=eval(input("enter the breadth of the rectanle is:"))
+area=l*b
+perimeter=2*(l+b)
+print("area of the rectanle is:",area)
+print("perimeter of the rectangle is:",perimeter)
+
